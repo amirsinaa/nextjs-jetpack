97b921927f06ea2d3c221885d6fee1b0574fd2a0
Amirsina <amirsina23@gmail.com>
2022-09-07 13:24:31 +0430

    -init: add a spinner component

---
+++ layouts/ui-parts/loading.tsx
@@ -0,0 +1,16 @@
+import { Center, Spinner } from "@chakra-ui/react";
+
+export const Loading = () => {
+  return (
+    <Center>
+      <Spinner
+        thickness="4px"
+        id="loadingSpinner"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="brand.primary"
+        size="xl"
+      />
+    </Center>
+  );
+};
